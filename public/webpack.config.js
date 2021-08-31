8b038392bbe170266ca7bc7bff2a85635634fcf1
Mike Steinmetz <mikesteinmetz00@gmail.com>
2021-08-30 22:18:31 -0500

    adding webpack

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,10 +1,31 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
 const config = {
     entry: "./src/app.js",
     output: {
         path: __dirname + "/dist",
         filename: "newBudgetTracker.js"
     },
-    mode: "development"
+    mode: "production",
+    plugins: [
+        new WebpackPwaManifest({
+            filename: 'manifest.json',
+            inject: false,
+            fingerprints: false,
+            name: "Budget Tracker App",
+            short_name: "Budget app",
+            background_color: "#fffff",
+            start_url: "/",
+            display: "standalone",
+            icons: [
+                {
+                src: path.resolve(__dirname, 'public/icons/icon-192x192.png'),
+                sizes: [192, 512]
+                }
+            ]
+        })
+    ]
 };
 
 module.exports = config;
